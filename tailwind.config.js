bf5a0c7aaf5d52b0b0f28fc1d5b14e2f459470b7
Wawa27 <32011804+Wawa27@users.noreply.github.com>
2024-06-06 16:10:40 +0200

    feat: add defaults for fonts, colors, text sizes  (#3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -9,7 +10,52 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Poppins"', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        'lg': ['1.25rem', {
+          lineHeight: '144%',
+          fontWeight: 'normal',
+        }],
+        'm': ['1.125rem', {
+          lineHeight: '132%',
+          fontWeight: 'light',
+        }],
+        'm-regular': ['1.125rem', {
+          lineHeight: '132%',
+          fontWeight: 'normal',
+        }],
+        's': ['1rem', {
+          lineHeight: '148%',
+          fontWeight: 'light',
+        }],
+        's-regular': ['1rem', {
+          lineHeight: '148%',
+          fontWeight: 'normal',
+        }],
+        'xs': ['0.875rem', {
+          lineHeight: '140%',
+          fontWeight: 'normal',
+        }],
+      },
+      color: {
+        'purple': "#9D3FE7",
+        'error': "#D51A52",
+        "black": "#1A141F",
+        "hint-text": "#4B3A5A",
+        "border": "#ABA7AF",
+        "disabled": "#D4D2D5",
+        "spacer": "#D9D1E0",
+        "spacer-light": "#E5E0EB",
+        "light-grey": "#F5F3F7",
+        "white": "#FFFFFF",
+        "attention": "#FF9500",
+        "approval": "#00B998",
+        "link": "#0F0BAB"
+      },
+    },
   },
   plugins: [],
 }
